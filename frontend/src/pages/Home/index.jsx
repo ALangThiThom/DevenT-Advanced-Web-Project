196639088be826403246aefc9dc02eb0bbaf00d0
ALangThiThom <alangthithom202@gmail.com>
2026-05-16 15:15:55 +0700

    build homepage footer component

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
+
 import Navbar from "../../components/layout/Navbar";
+import Footer from "../../components/layout/Footer";
 export default function Homepage() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
