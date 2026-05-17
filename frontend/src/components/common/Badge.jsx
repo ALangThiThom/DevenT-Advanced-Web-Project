0c47c574473cd99918f9c3fa06868262efbd6051
ALangThiThom <alangthithom202@gmail.com>
2026-05-17 20:38:40 +0700

    delete two components: badge.jsx and button.jsx

--- frontend/src/components/common/Badge.jsx
+++
@@ -1,3 +0,0 @@
- export const Badge = ({ label, color = "green" }) => (
-  <span className={`badge badge-${color}`}>{label}</span>
-);
--- frontend/src/components/common/Button.jsx
+++
@@ -1,8 +0,0 @@
-const Button = ({ children, variant = "primary", onClick, ...props }) => {
-  return (
-    <button className={`btn btn-${variant}`} onClick={onClick} {...props}>
-      {children}
-    </button>
-  );
-};
-export default Button;
