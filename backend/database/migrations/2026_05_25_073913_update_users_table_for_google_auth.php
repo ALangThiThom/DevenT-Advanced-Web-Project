4af0ea36adcfcff837ec74f415de396f4176a581
danhwn06-be <st.anhdan09@gmail.com>
2026-05-25 14:55:52 +0700

    feat(backend): install socialite and update users table for google auth (ECP-75)

---
+++ backend/database/migrations/2026_05_25_073913_update_users_table_for_google_auth.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // 1. Thêm cột google_id
+            $table->string('google_id')->nullable()->after('email');
+
+            // 2. Sửa cột password
+            $table->string('password')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // 1. Xóa cột google_id
+            $table->dropColumn('google_id');
+
+            // 2. Khôi phục cột password
+            $table->string('password')->nullable(false)->change();
+        });
+    }
+};
